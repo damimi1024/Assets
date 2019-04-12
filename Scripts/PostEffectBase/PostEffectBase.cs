80dc724e95478600345f46aa1c0726b1d5b947ee
yry <1002131619@qq.com>
2019-04-12 17:28:23 +0800

    outline3
    外边缘发光 后处理模式

---
+++ Scripts/PostEffectBase/PostEffectBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//非运行时也触发效果
+[ExecuteInEditMode]
+
+[RequireComponent(typeof(Camera))]
+//提供一个后处理的基类，主要功能在于直接通过Inspector面板拖入shader，生成shader对应的材质
+public class PostEffectBase : MonoBehaviour
+{
+
+    public Shader shader = null;
+    private Material _material;
+    public Material _Material
+    {
+        get
+        {
+            if (_material == null)
+                _material = GenerateMaterial(shader);
+            return _material;
+        }
+    }
+
+    private Material GenerateMaterial(Shader shader)
+    {
+        if (shader == null)
+            return null;
+        //需要判断shader是否支持
+        if (shader.isSupported == false)
+            return null;
+        Material material = new Material(shader);
+        material.hideFlags = HideFlags.DontSave;
+        if (material)
+            return material;
+        return null;
+    }
+}
---
+++ Scripts/renderWithShader/renderShaderClassTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class renderShaderClassTest : MonoBehaviour
+{
+    bool is_use = false;
+    void OnGUI()
+    {
+        if (is_use)
+        {
+            //使用高光shader：Specular来渲染Camera
+            GetComponent<Camera>().RenderWithShader(Shader.Find("Specular"), "RenderType");
+        }
+        if (GUI.Button(new Rect(10.0f, 10.0f, 300.0f, 45.0f), "使用RenderWithShader启用高光"))
+        {
+            //RenderWithShader每调用一次只渲染一帧，所以不可将其直接放到这儿
+            //camera.RenderWithShader(Shader.Find("Specular"), "RenderType")
+            is_use = true;
+        }
+        if (GUI.Button(new Rect(10.0f, 60.0f, 300.0f, 45.0f), "使用SetReplacementShader启用高光"))
+        {
+            //SetReplacementShader方法用来替换已有shader，调用一次即可
+            GetComponent<Camera>().SetReplacementShader(Shader.Find("Specular"), "RenderType");
+            is_use = false;
+        }
+        if (GUI.Button(new Rect(10.0f, 110.0f, 300.0f, 45.0f), "关闭高光"))
+        {
+            //重置摄像机的shader渲染模式
+            GetComponent<Camera>().ResetReplacementShader();
+            is_use = false;
+        }
+    }
+    
+}
