921af87cf32d4868ea1f8ce81c7f2527ab40c403
杨若延 <33301027+damimi1024@users.noreply.github.com>
2021-06-17 18:17:11 +0800

    11

---
+++ Resources/Material/GpuInstance/GpuInstance.shader
@@ -0,0 +1,72 @@
+﻿Shader "Unlit/GpuInstance"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _BaseColor ("_BaseColor", Color) = (1,1,1,1)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            #include "AutoLight.cginc"
+            #pragma multi_compile_instancing
+
+            UNITY_INSTANCING_BUFFER_START(Props)
+            UNITY_DEFINE_INSTANCED_PROP(float4, _MainColor)
+            UNITY_DEFINE_INSTANCED_PROP(fixed, _Alpha)
+            UNITY_INSTANCING_BUFFER_END(Props)
+            half4 _BaseColor;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                float3 normal : NORMAL;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float3 normal_world :TEXCOORD1;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                 UNITY_SETUP_INSTANCE_ID(v);
+                 UNITY_TRANSFER_INSTANCE_ID(v, o);
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.normal_world = mul(v.normal,unity_WorldToObject);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                UNITY_SETUP_INSTANCE_ID(i);
+                half3 light_dir = normalize(_WorldSpaceLightPos0.xyz);
+                half3 nDotL = normalize(dot(i.normal_world,light_dir));
+                fixed4 col = tex2D(_MainTex, i.uv);
+                half3 diffuse_color = col.xyz * nDotL * _BaseColor.xyz * UNITY_ACCESS_INSTANCED_PROP(Props, _MainColor);
+                // sample the texture
+                return fixed4(diffuse_color,1);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Scripts/Sort/UIEffectRenderer.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEffectRenderer : MonoBehaviour
+{
+    public static void SortRenderersInChildren(Transform trans, int sortingOrder)
+    {
+        var effectRenderer = trans.GetComponent<UIEffectRenderer>();
+        Renderer[] renderers;
+        if (effectRenderer != null)
+        {
+            renderers = effectRenderer.renders;
+        }
+        else
+        {
+            renderers = trans.GetComponentsInChildren<Renderer>(true);
+        }
+        SortSortingOrderAll(renderers, sortingOrder);
+    }
+
+    public static void SortSortingOrderAll(Renderer[] renders, int sortingOrder)
+    {
+        if (renders != null)
+        {
+            for (int i = renders.Length - 1; i >= 0; i--)
+            {
+                var renderer = renders[i];
+                if (renderer != null)
+                {
+                    renderer.sortingOrder = sortingOrder;
+                }
+            }
+        }
+    }
+
+    public static void SortCanvas(Transform trans, int sortingOrder)
+    {
+        var canvas = trans.GetComponent<Canvas>();
+        if (canvas == null)
+            canvas = trans.gameObject.AddComponent<Canvas>();
+        canvas.sortingOrder = sortingOrder;
+    }
+
+    public Renderer[] renders;
+
+    public bool enableAutoDepth = true;
+
+    void OnEnable()
+    {
+        if (enableAutoDepth)
+        {
+            var parentCanvas = this.GetComponentInParent<Canvas>();
+            if (parentCanvas)
+            {
+                var parentDepth = GetComponentInParentRange<UIConstDepth>(this.transform, parentCanvas.transform);
+                if (parentDepth == null)
+                {
+                    parentDepth = parentCanvas.gameObject.AddComponent<UIConstDepth>();
+                    parentDepth.isUI = false;
+                    parentDepth.Init();
+                }
+                parentDepth.DoEffectSorting(this);
+            }
+            else
+            {
+                // 无法排序还是报个错, 以方便后续分析问题
+                //Debug.LogError("无法排序节点" + this.name, this);
+            }
+        }
+    }
+
+    public void Save()
+    {
+        renders = GetComponentsInChildren<Renderer>(true);
+    }
+
+    private void Reset()
+    {
+        renders = GetComponentsInChildren<Renderer>(true);
+    }
+
+    // 获取到指定位置的指定组件
+    private static T GetComponentInParentRange<T>(Transform startTrans, Transform endTrans) where T : Component
+    {
+        var pT = startTrans.GetComponentInParent<T>();
+        if (pT != null)
+        {
+            // 如果2个不相同, 则获取一个最近的
+            var pTrans = pT.transform;
+            var p = startTrans;
+            while (p != null)
+            {
+                // 找到了
+                if (p == pTrans)
+                {
+                    return pT;
+                }
+                // 结束点, 应该停止了
+                else if (p == endTrans)
+                {
+                    return null;
+                }
+                // 下一个节点判断
+                else
+                {
+                    p = p.parent;
+                }
+            }
+        }
+        return null;
+    }
+}
