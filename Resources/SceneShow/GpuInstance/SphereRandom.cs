4499469d1bf7ff51e045e3e40c286864cad8fdc5
杨若延 <33301027+damimi1024@users.noreply.github.com>
2021-07-12 18:23:43 +0800

    gpuinstance

---
+++ Resources/SceneShow/GpuInstance/SphereRandom.cs
@@ -0,0 +1,61 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+public class SphereRandom : MonoBehaviour
+{
+    public Camera camera;
+    /// <summary> 
+    /// /// 父级 -- 球  
+    /// /// </summary> 
+    public Transform parent;
+    /// /// <summary> 
+    /// /// 预设 -- 球  
+    /// /// </summary> 
+    public GameObject prefab; 
+    public Transform[] kids;
+    // Use this for initialization  
+    void Start()
+    {
+        CalualteSphere();
+    }
+    // Update is called once per frame 
+    void Update()
+    {
+        if (!Input.GetMouseButton(0)) return;
+        float fMouseX = Input.GetAxis("Mouse X");
+        float fMouseY = Input.GetAxis("Mouse Y");
+        parent.Rotate(Vector3.up, -fMouseX * 2, Space.World);
+        parent.Rotate(Vector3.right, fMouseY * 2, Space.World);
+        for (int i = 0; i < kids.Length; i++) 
+            kids[i].LookAt(camera.transform);
+    }
+    /// <summary> /// 平均分成的等份 
+    /// </summary> 
+    int N = 200;
+    /// /// <summary> /// 小球的半径  
+    float size = 10f;
+    /// /// <summary> /// 球体表面平均分割点 
+    /// /// </summary> 
+    void CalualteSphere()
+    {
+        float inc = Mathf.PI * (3.0f - Mathf.Sqrt(5.0f));
+        float off = 2.0f / N;
+        //注意保持数值精度  
+        kids = new Transform[N];
+        for (int i = 0; i < N; i++)
+        {
+            float y = (float)i * off - 1.0f + (off / 2.0f);
+            float r = Mathf.Sqrt(1.0f - y * y);
+            float phi = i * inc;
+            Vector3 pos = new Vector3(Mathf.Cos(phi) * r * size,    y * size,    Mathf.Sin(phi) * r * size);
+            GameObject tempGo = Instantiate(prefab) as GameObject;
+            tempGo.transform.parent = parent;
+            tempGo.transform.localScale = new Vector3(1, 1, 1);
+            tempGo.transform.localPosition = pos;
+            tempGo.SetActive(true);
+            kids[i] = tempGo.transform;
+        }
+    }
+
+}
--- Scripts/GPUInstanceTest/GPUInstanceTest.cs
+++ Scripts/GPUInstanceTest/GPUInstanceTest.cs
@@ -8,6 +8,11 @@ public class GPUInstanceTest : MonoBehaviour
     public Mesh instanceMesh;//参数输入：模型
     Matrix4x4[] matrices;//绘制位置
     MaterialPropertyBlock block;
+    public float Range = 10;
+    private Vector3 targetPoint = new Vector3();
+    private Vector3 movePoint = new Vector3();
+
+    private Vector3 SphereCenter = new Vector3();
 
     void BuildMatrixAndBlock()
     {
@@ -20,7 +25,8 @@ public class GPUInstanceTest : MonoBehaviour
             {
                 var ind = j * 32 + i;
                 if (ind >= 1023) break;
-                matrices[ind] = Matrix4x4.TRS(new Vector3(i, j, 0), Quaternion.identity, Vector3.one);
+                Vector3 range = CalualteSphere(ind,1023,2,Vector3.zero);// Random.insideUnitSphere
+                matrices[ind] = Matrix4x4.TRS(range * Range, Quaternion.identity, Vector3.one);
                 colors[ind] = new Vector4(1 - i / 32.0f, 1 - j / 32.0f, 1, 1);
 
             }
@@ -59,10 +65,50 @@ public class GPUInstanceTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //需要碰撞到物体才可以
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool isCollider = Physics.Raycast(ray, out hit);
+            if (isCollider)
+            {
+                Debug.Log("射线检测到的点是" + hit.point);
+            }
+            targetPoint = hit.point;
+        }
+        if (targetPoint != Vector3.zero){
+
+            for (int i = 0; i < matrices.Length; i++)
+            {
+
+                var position = new Vector3(matrices[i][0, 3], matrices[i][1, 3], matrices[i][2, 3]);
+                movePoint = Vector3.Lerp(position, targetPoint, 0.1f);
+                Vector3 range = CalualteSphere(i, 1023, 2, movePoint);// Random.insideUnitSphere
+                matrices[i].SetTRS(range, Quaternion.identity, Vector3.one);
+            }
+        }
+        //for (int i = 0; i < matrices.Length; i++)
+        //{
+        //    var position = new Vector3(matrices[i][0, 3], matrices[i][1, 3], matrices[i][2, 3]);
+        //    movePoint = Vector3.Lerp(targetPoint, position, 0.1f);
+        //    matrices[i].SetTRS(position + movePoint, Quaternion.identity, Vector3.one);
+        //}
         if (!option)
         {
             Graphics.DrawMeshInstanced(instanceMesh, 0, instanceMat, matrices, 1023, block, UnityEngine.Rendering.ShadowCastingMode.Off, false);
         }
     }
 
+
+    Vector3 CalualteSphere(int index, int count, int radius,Vector3 offset)
+    {
+        float inc = Mathf.PI * (3.0f - Mathf.Sqrt(5.0f));
+        float off = 2.0f / count;
+        float y = (float)index * off - 1.0f + (off / 2.0f);
+        float r = Mathf.Sqrt(1.0f - y * y);
+        float phi = index * inc;
+        Vector3 pos = new Vector3(Mathf.Cos(phi) * r * radius + offset.x, y * radius+offset.y, Mathf.Sin(phi) * r * radius + offset.z);
+        return pos;
+    }
 }
